710ae4e94abc50135105ca5311b15736cd76a2c3
fjplutng <fjplutng@hotmail.com>
2016-02-07 15:28:33 -0600

    7

---
+++ share/ejercicio1/src/fjplutng/giti7083s/animal/Animal.java
@@ -0,0 +1,10 @@
+package fjplutng.giti7083s.animal;
+
+public interface Animal {
+	/**
+	 * CREA LOS MÉTODOS DE LA INTERFASE
+	 */
+	public void dibuja();
+	public void imprime();
+
+}
---
+++ share/ejercicio1/src/fjplutng/giti7083s/animal/Leon.java
@@ -0,0 +1,19 @@
+package fjplutng.giti7083s.animal;
+
+public class Leon implements Animal {
+	/**
+	 * IMPLEMENTA LOS MÉTODOS DE LA INTERFASE RELACIONADA
+	 */
+	@Override
+	public void dibuja() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void imprime() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
